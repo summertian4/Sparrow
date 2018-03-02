a79a2eadf20abc5d48daa862b5ab8b622be6af69
小鱼周凌宇 <coderfish@163.com>
2018-03-02 14:29:05 +0800

    frontend-项目列表分页

--- Sparrow/frontend/client/views/project/index.vue
+++ Sparrow/frontend/client/views/project/index.vue
@@ -6,35 +6,46 @@
       </router-link>
     </div>
 
-    <div class="tile is-ancestor" v-for="(project, index) in projects" v-if="index % 4 === 0">
-      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projects[index].project_id}" append
-                   v-if="index < projects.length">
+    <div class="tile is-ancestor" v-for="(project, index) in projectsData.projects" v-if="index % 4 === 0">
+      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projectsData.projects[index].project_id}" append
+                   v-if="index < projectsData.projects.length">
         <article class="tile is-child box">
-          <h4 class="title">{{ projects[index].name }}</h4>
-          <div class="note">{{ projects[index].note }}</div>
+          <h4 class="title">{{ projectsData.projects[index].name }}</h4>
+          <div class="note">{{ projectsData.projects[index].note }}</div>
         </article>
       </router-link>
-      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projects[index + 1].project_id}" append
-                   v-if="index + 1 < projects.length">
+      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projectsData.projects[index + 1].project_id}"
+                   append
+                   v-if="index + 1 < projectsData.projects.length">
         <article class="tile is-child box">
-          <h4 class="title">{{ projects[index + 1].name }}</h4>
-          <div class="note">{{ projects[index + 1].note }}</div>
+          <h4 class="title">{{ projectsData.projects[index + 1].name }}</h4>
+          <div class="note">{{ projectsData.projects[index + 1].note }}</div>
         </article>
       </router-link>
-      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projects[index + 2].project_id}" append
-                   v-if="index + 2 < projects.length">
+      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projectsData.projects[index + 2].project_id}"
+                   append
+                   v-if="index + 2 < projectsData.projects.length">
         <article class="tile is-child box">
-          <h4 class="title">{{ projects[index + 2].name }}</h4>
-          <div class="note">{{ projects[index + 2].note }}</div>
+          <h4 class="title">{{ projectsData.projects[index + 2].name }}</h4>
+          <div class="note">{{ projectsData.projects[index + 2].note }}</div>
         </article>
       </router-link>
-      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projects[index + 3].project_id}" append
-                   v-if="index + 3 < projects.length">
+      <router-link class="tile is-parent is-3" :to="{ path: 'detail/'+projectsData.projects[index + 3].project_id}"
+                   append
+                   v-if="index + 3 < projectsData.projects.length">
         <article class="tile is-child box">
-          <h4 class="title">{{ projects[index + 3].name }}</h4>
-          <div class="note">{{ projects[index + 3].note }}</div>
+          <h4 class="title">{{ projectsData.projects[index + 3].name }}</h4>
+          <div class="note">{{ projectsData.projects[index + 3].note }}</div>
         </article>
       </router-link>
+    </div>
+    <div class="pagecontrol right">
+      <el-pagination
+        layout="prev, pager, next"
+        :page-size="projectsData.limit"
+        :total="projectsData.total"
+        @current-change="pageChange">
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -43,30 +54,42 @@
   import Chart from 'vue-bulma-chartjs'
   import {request} from '../network.js'
   import * as notification from '../notification.js'
+  import ElPagination from 'element-pagination'
 
   export default {
     components: {
-      Chart
+      Chart,
+      ElPagination
     },
 
     data () {
       return {
         hello: 'hello',
-        projects: ''
+        projectsData: {
+          total: 0,
+          current_page: 0,
+          limit: 0,
+          projects: []
+        }
       }
     },
 
     created () {
-      this.loadProjects()
+      this.loadProjects(1)
     },
 
     computed: {},
 
     methods: {
-      loadProjects () {
-        request('/frontend/project/list')
+      loadProjects (page) {
+        request('/frontend/project/list', {
+          params: {
+            current_page: page,
+            limit: 12
+          }
+        })
           .then((data) => {
-            this.projects = data['projects']
+            this.projectsData = data['projects_data']
           })
           .catch((data) => {
             notification.toast({
@@ -75,6 +98,10 @@
               duration: 2000
             })
           })
+      },
+
+      pageChange (currentPage) {
+        this.loadProjects(currentPage)
       }
     }
   }
@@ -92,5 +119,9 @@
 
   .note {
     height: 80px;
+  }
+
+  .pagecontrol {
+    background: #FFFFFF;
   }
 </style>
