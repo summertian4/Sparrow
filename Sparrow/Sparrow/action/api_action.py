565144d6e97527193d2f88353743c8a4ec9bab62
小鱼周凌宇 <coderfish@163.com>
2018-03-02 13:58:21 +0800

    backend-Method 校验

--- Sparrow/Sparrow/action/api_action.py
+++ Sparrow/Sparrow/action/api_action.py
@@ -14,6 +14,10 @@ Success = 200
 
 class ApiAction:
     def list(request, project_id):
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
         limit = 10
         page = 1
         if 'limit' in request.GET.keys():
@@ -43,64 +47,68 @@ class ApiAction:
 
     @csrf_exempt
     def create(request, project_id):
-        if request.method == CommonData.Method.POST.value:
-            form = ApiCreateForm(data=request.POST)
-            # check whether it's valid:
-            if form.is_valid():
-                model = Api()
-                model.path = form.clean().get('path')
-                model.method = form.clean().get('method')
-                model.name = form.clean().get('name')
-                model.note = form.clean().get('note')
-                model.status = form.clean().get('status')
-                model.responseJson = form.clean().get('responseJson')
-                api = ApiDao.create(model)
-                project = ProjectDao.get_project_with_id(project_id)
-                project.apis.add(api)
-                project.save()
-                if api is None:
-                    data = CommonData.response_data(DaoOperationError, "API create faild")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
-                else:
-                    data = CommonData.response_data(Success, "sucsses")
-                    data['api'] = model_to_dict(api)
-                    return HttpResponse(json.dumps(data), content_type="application/json")
+        if request.method != CommonData.Method.POST.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
+        form = ApiCreateForm(data=request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            model = Api()
+            model.path = form.clean().get('path')
+            model.method = form.clean().get('method')
+            model.name = form.clean().get('name')
+            model.note = form.clean().get('note')
+            model.status = form.clean().get('status')
+            model.responseJson = form.clean().get('responseJson')
+            api = ApiDao.create(model)
+            project = ProjectDao.get_project_with_id(project_id)
+            project.apis.add(api)
+            project.save()
+            if api is None:
+                data = CommonData.response_data(DaoOperationError, "API create faild")
+                return HttpResponse(json.dumps(data), content_type="application/json")
             else:
-                data = CommonData.response_data(FormParseError, "form parse faild")
+                data = CommonData.response_data(Success, "sucsses")
+                data['api'] = model_to_dict(api)
                 return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(FormParseError, "form parse faild")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     def repeat_name_verification(request, project_id):
-        if request.method == 'GET':
-            if ('path' not in request.GET.keys()) or \
-                    ('method' not in request.GET.keys()):
-                data = CommonData.response_data(MissingParametersError, "缺少参数")
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            path = request.GET['path']
-            if path is None:
-                return
-            api = ApiDao.get_apis_with_project_id_and_path(project_id, path).first()
-            data = CommonData.response_data(Success, "Success")
-            if api is None:
-                data['repeatability'] = False
-            else:
-                data['api'] = api.as_dict()
-                if 'api_id' in request.GET.keys():
-                    api_id = request.GET['api_id']
-                    if (str(api.api_id) == str(api_id)):
-                        data['repeatability'] = False
-                    else:
-                        data['repeatability'] = True
-                else:
-                    data['repeatability'] = True
-            return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
-        else:
-            data = CommonData.response_data(RequetMethodError, "POST is invalid")
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
+        if ('path' not in request.GET.keys()) or \
+                ('method' not in request.GET.keys()):
+            data = CommonData.response_data(MissingParametersError, "缺少参数")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+        path = request.GET['path']
+        if path is None:
+            return
+        api = ApiDao.get_apis_with_project_id_and_path(project_id, path).first()
+        data = CommonData.response_data(Success, "Success")
+        if api is None:
+            data['repeatability'] = False
+        else:
+            data['api'] = api.as_dict()
+            if 'api_id' in request.GET.keys():
+                api_id = request.GET['api_id']
+                if (str(api.api_id) == str(api_id)):
+                    data['repeatability'] = False
+                else:
+                    data['repeatability'] = True
+            else:
+                data['repeatability'] = True
+        return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
+
     def detail(request, project_id, api_id):
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
         isOriginal = True
         if 'isOriginal' in request.GET.keys():
             isOriginal = False
@@ -113,65 +121,69 @@ class ApiAction:
 
     @csrf_exempt
     def update(request, project_id, api_id):
-        if request.method == CommonData.Method.POST.value:
-            form = ApiUpdateForm(data=request.POST)
-            if form.is_valid():
-                model = ApiDao.get_api_with_id(api_id)
-                model.path = form.clean().get('path')
-                model.method = form.clean().get('method')
-                model.name = form.clean().get('name')
-                model.note = form.clean().get('note')
-                model.status = form.clean().get('status')
-                model.responseJson = form.clean().get('responseJson')
-                result = ApiDao.update(model)
+        if request.method != CommonData.Method.POST.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
 
-                if result is False:
-                    data = CommonData.response_data(DaoOperationError, "API update faild")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
-                else:
-                    data = CommonData.response_data(Success, "Sucsses")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
+        form = ApiUpdateForm(data=request.POST)
+        if form.is_valid():
+            model = ApiDao.get_api_with_id(api_id)
+            model.path = form.clean().get('path')
+            model.method = form.clean().get('method')
+            model.name = form.clean().get('name')
+            model.note = form.clean().get('note')
+            model.status = form.clean().get('status')
+            model.responseJson = form.clean().get('responseJson')
+            result = ApiDao.update(model)
+
+            if result is False:
+                data = CommonData.response_data(DaoOperationError, "API update faild")
+                return HttpResponse(json.dumps(data), content_type="application/json")
             else:
-                data = CommonData.response_data(FormParseError, "Form parse faild")
+                data = CommonData.response_data(Success, "Sucsses")
                 return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(FormParseError, "Form parse faild")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     def delete(request, project_id, api_id):
-        if request.method == 'GET':
-            succesed = ApiDao.delete(api_id)
-            if succesed:
-                data = CommonData.response_data(Success, "Success")
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            else:
-                data = CommonData.response_data(DaoOperationError, "Delete Failed")
-                return HttpResponse(json.dumps(data), content_type="application/json")
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
+        succesed = ApiDao.delete(api_id)
+        if succesed:
+            data = CommonData.response_data(Success, "Success")
+            return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(DaoOperationError, "Delete Failed")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     def star(request, project_id, api_id):
-        if request.method == 'GET':
-            api = ApiDao.get_api_with_id(api_id)
-            if api is not None:
-                api.star = not api.star
-                print(api.star)
-                result = ApiDao.update(api)
-                if result is False:
-                    data = CommonData.response_data(DaoOperationError, "Update API Faild")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
-                else:
-                    data = CommonData.response_data(Success, "Success")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
+        api = ApiDao.get_api_with_id(api_id)
+        if api is not None:
+            api.star = not api.star
+            print(api.star)
+            result = ApiDao.update(api)
+            if result is False:
+                data = CommonData.response_data(DaoOperationError, "Update API Faild")
+                return HttpResponse(json.dumps(data), content_type="application/json")
             else:
-                data = CommonData.response_data(DaoOperationError, "API is not exist")
+                data = CommonData.response_data(Success, "Success")
                 return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(DaoOperationError, "API is not exist")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     def starList(request):
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
         limit = 10
         page = 1
         if 'limit' in request.GET.keys():
@@ -181,22 +193,18 @@ class ApiAction:
 
         offset = (page - 1) * limit
 
-        if request.method == 'GET':
-            apis = ApiDao.get_all_stared_apis(offset, limit)
-            apis_dict = []
-            for api in apis:
-                api_dic = api.as_dict()
-                project_dic = ProjectDao.get_project_with_api_id(api.api_id)
-                if project_dic is not None:
-                    api_dic['project'] = project_dic
-                apis_dict.append(api_dic)
-            count = ApiDao.get_stared_apis_count()
-            data = CommonData.response_data(Success, "Success")
-            data["apis_data"] = {"apis": apis_dict,
-                                 "current_page": page,
-                                 "total": count,
-                                 "limit": limit}
-            return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
-        else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
-            return HttpResponse(json.dumps(data), content_type="application/json")
+        apis = ApiDao.get_all_stared_apis(offset, limit)
+        apis_dict = []
+        for api in apis:
+            api_dic = api.as_dict()
+            project_dic = ProjectDao.get_project_with_api_id(api.api_id)
+            if project_dic is not None:
+                api_dic['project'] = project_dic
+            apis_dict.append(api_dic)
+        count = ApiDao.get_stared_apis_count()
+        data = CommonData.response_data(Success, "Success")
+        data["apis_data"] = {"apis": apis_dict,
+                             "current_page": page,
+                             "total": count,
+                             "limit": limit}
+        return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
--- Sparrow/Sparrow/action/project_action.py
+++ Sparrow/Sparrow/action/project_action.py
@@ -31,90 +31,90 @@ class ProjectAction:
 
     @csrf_exempt
     def create(request):
-        if request.method == CommonData.Method.POST.value:
-            form = ProjectCreateForm(data=request.POST)
-            if form.is_valid():
-                model = Project()
-                model.name = form.clean().get('name')
-                model.note = form.clean().get('note')
-                model.status = form.clean().get('status')
-                project = ProjectDao.create(model)
-                if project is None:
-                    data = CommonData.response_data(DaoOperationError, "API create faild")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
-                else:
-                    data = CommonData.response_data(Success, "sucsses")
-                    data['project'] = model_to_dict(project)
-                    return HttpResponse(json.dumps(data), content_type="application/json")
+        if request.method != CommonData.Method.POST.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
+        form = ProjectCreateForm(data=request.POST)
+        if form.is_valid():
+            model = Project()
+            model.name = form.clean().get('name')
+            model.note = form.clean().get('note')
+            model.status = form.clean().get('status')
+            project = ProjectDao.create(model)
+            if project is None:
+                data = CommonData.response_data(DaoOperationError, "API create faild")
+                return HttpResponse(json.dumps(data), content_type="application/json")
             else:
-                data = CommonData.response_data(FormParseError, "form parse faild")
+                data = CommonData.response_data(Success, "sucsses")
+                data['project'] = model_to_dict(project)
                 return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(FormParseError, "form parse faild")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     @csrf_exempt
     def update(request, project_id):
-        if request.method == CommonData.Method.POST.value:
-            form = ProjectUpateForm(data=request.POST)
-            if form.is_valid():
-                model = ProjectDao.get_project_with_id(project_id)
-                model.name = form.clean().get('name')
-                model.note = form.clean().get('note')
-                model.status = form.clean().get('status')
-                result = ProjectDao.update(model)
+        if request.method != CommonData.Method.POST.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
 
-                if result is False:
-                    data = CommonData.response_data(DaoOperationError, "Project update faild")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
-                else:
-                    data = CommonData.response_data(Success, "sucsses")
-                    return HttpResponse(json.dumps(data), content_type="application/json")
+        form = ProjectUpateForm(data=request.POST)
+        if form.is_valid():
+            model = ProjectDao.get_project_with_id(project_id)
+            model.name = form.clean().get('name')
+            model.note = form.clean().get('note')
+            model.status = form.clean().get('status')
+            result = ProjectDao.update(model)
+
+            if result is False:
+                data = CommonData.response_data(DaoOperationError, "Project update faild")
+                return HttpResponse(json.dumps(data), content_type="application/json")
             else:
-                data = CommonData.response_data(FormParseError, "Form parse faild")
+                data = CommonData.response_data(Success, "sucsses")
                 return HttpResponse(json.dumps(data), content_type="application/json")
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data = CommonData.response_data(FormParseError, "Form parse faild")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     def repeat_name_verification(request):
-        if request.method == 'GET':
-            if ('name' not in request.GET.keys()):
-                data = CommonData.response_data(MissingParametersError, "缺少参数")
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            name = request.GET['name']
-            project = ProjectDao.get_project_with_Name(name)
-            data = CommonData.response_data(Success, "Success")
-            if project is None:
-                data['repeatability'] = False
-            else:
-                data['api'] = project.as_dict()
-                if 'project_id' in request.GET.keys():
-                    project_id = request.GET['project_id']
-                    if str(project.project_id) == str(project_id):
-                        data['repeatability'] = False
-                    else:
-                        data['repeatability'] = True
-                else:
-                    data['repeatability'] = True
-            return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
-        else:
-            data = CommonData.response_data(RequetMethodError, "POST is invalid")
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
             return HttpResponse(json.dumps(data), content_type="application/json")
 
-    def delete(request):
-        if request.method == 'GET':
-            project_id = request.GET['project_id']
-            if project_id is None:
-                data = CommonData.response_data(RequetParamsError, "project_id is None")
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            succesed = ProjectDao.delete(project_id)
-            if succesed:
-                data = CommonData.response_data(Success, "Success")
-                return HttpResponse(json.dumps(data), content_type="application/json")
-            else:
-                data = CommonData.response_data(DaoOperationError, "Delete Failed")
-                return HttpResponse(json.dumps(data), content_type="application/json")
+        if ('name' not in request.GET.keys()):
+            data = CommonData.response_data(MissingParametersError, "缺少参数")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+        name = request.GET['name']
+        project = ProjectDao.get_project_with_Name(name)
+        data = CommonData.response_data(Success, "Success")
+        if project is None:
+            data['repeatability'] = False
         else:
-            data = CommonData.response_data(RequetMethodError, "GET is invalid")
+            data['api'] = project.as_dict()
+            if 'project_id' in request.GET.keys():
+                project_id = request.GET['project_id']
+                if str(project.project_id) == str(project_id):
+                    data['repeatability'] = False
+                else:
+                    data['repeatability'] = True
+            else:
+                data['repeatability'] = True
+        return HttpResponse(json.dumps(data, default=datetime2string), content_type="application/json")
+
+    def delete(request):
+        if request.method != CommonData.Method.GET.value:
+            data = CommonData.response_data(RequetMethodError, "Method is invalid")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+
+        project_id = request.GET['project_id']
+        if project_id is None:
+            data = CommonData.response_data(RequetParamsError, "project_id is None")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+        succesed = ProjectDao.delete(project_id)
+        if succesed:
+            data = CommonData.response_data(Success, "Success")
+            return HttpResponse(json.dumps(data), content_type="application/json")
+        else:
+            data = CommonData.response_data(DaoOperationError, "Delete Failed")
             return HttpResponse(json.dumps(data), content_type="application/json")
